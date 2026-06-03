363bd63f5073a90dd984619b18b2ebd14d567e4e
Alvaro Otero Cobo <alvarootero83m@gmail.com>
2026-06-03 15:18:09 +0200

    Add files via upload

--- chatbot_web.py
+++ chatbot_web.py
@@ -846,9 +846,9 @@ HTML = '''
             botMsg.className = 'message bot-message';
             
             if (data.mensaje) {
-                botMsg.innerHTML = data.mensaje + '\n\n🔄 Puedes hacer una nueva reserva. Por favor, ingresa tu número de teléfono:';
+                botMsg.innerHTML = data.mensaje + ' 🔄 Puedes hacer una nueva reserva. Por favor, ingresa tu número de teléfono:';
             } else {
-                botMsg.innerHTML = '✅ Reserva cancelada correctamente\n\n🔄 Puedes hacer una nueva reserva. Por favor, ingresa tu número de teléfono:';
+                botMsg.innerHTML = '✅ Reserva cancelada correctamente. 🔄 Puedes hacer una nueva reserva. Por favor, ingresa tu número de teléfono:';
             }
             
             chatMessages.appendChild(botMsg);
